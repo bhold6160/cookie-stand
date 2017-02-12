2628ce583b810689f31e4386f86e20b6cc4c4546
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-02-11 17:27:15 -0800

    items will list

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
+var hours = ['10am','11am','12pm','1pm','2pm','3pm','4pm','5pm',];
 
-var pikePlace = {
-  min: 1,
-  max: 2,
-  avg: 3,
-  // What other properties might you need to keep track of?
-  getRandom: function(min, max) {
-    return Math.random() * ((max - min) + min);
+var pikePlaceMarket = {
+  minPeeps: 17,
+  maxPeeps: 88,
+  avgCookies: 5.2,
+  total: [],
+
+customerPerHour: function(min, max) {
+  return Math.random() * (this.maxPeeps - this.minPeeps + 1) + this.minPeeps;
+},
+
+cookiesPerHour: function() {
+  for(i = 0; i < hours.length; i++) {
+    var totalCookies = Math.floor(this.customerPerHour() * this.avgCookies);
+    this.total.push(totalCookies)
+    // this.total +=
+    console.log(this.total)
+    }
   },
-  // other methods to aggregate data...
-  render: function() {
-    // render your object data to the DOM as an unordered list of data points!!
-    // 1. Get an element by Id from your html
-    // 2. Create a UL element
-    // 3. Create and populate an LI element for each of the data points that you want to render
-    // 4. Append the LI to the UL
-    // 5. Append the UL to the element that you stored in step 1
+
+// this.render = function() {
+// var storeLocation = document.getElementById('pike-place')
+// var liElement = document.createElement()
+// }
+
+render: function() {
+var ul = document.getElementById('pike-place');
+var child;
+var total = 0;
+
+for (i = 0; i < 8; i++) {
+  var li = document.createElement('li');
+ li.textContent = hours[i] + ' ' + this.total[i] + ' cookies';
+ ul.appendChild(li);
+
+
+
+
+
+
+
+//   if (i < 2) {
+//   child.textContent = ( (10 + i) +  ' am:' + ' ' + pikePlace.cookiesPerHour[i] + ' cookies');
+// } else if (i === 2) {
+//   cild.textContent = ('12 pm:' + ' ' + pikePlace.cookiesPerHour[i] + ' cookies');
+// } else {
+//   cild.textContent = ((i - 2) + ' pm:' + ' ' + pikePlace.cookiesPerHour[i] + ' cookies');
+// }
+//
+// total += pikePlace.cookiesPerHour[i];
+// parent.appendChild(child);
+// console.log(total);
+    }
   }
-};
+}
+
+pikePlaceMarket.render()
