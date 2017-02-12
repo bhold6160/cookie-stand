4bff3b5acc5982500ca7f93fa9d257563b37509d
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-02-11 17:51:30 -0800

    list items showing correctly

--- app.js
+++ app.js
@@ -14,45 +14,20 @@ cookiesPerHour: function() {
   for(i = 0; i < hours.length; i++) {
     var totalCookies = Math.floor(this.customerPerHour() * this.avgCookies);
     this.total.push(totalCookies)
-    // this.total +=
     console.log(this.total)
     }
   },
 
-// this.render = function() {
-// var storeLocation = document.getElementById('pike-place')
-// var liElement = document.createElement()
-// }
-
 render: function() {
-var ul = document.getElementById('pike-place');
-var child;
-var total = 0;
-
-for (i = 0; i < 8; i++) {
+  var ulElement = document.getElementById('pike-place');
+  for (i = 0; i < 8; i++) {
   var li = document.createElement('li');
- li.textContent = hours[i] + ' ' + this.total[i] + ' cookies';
- ul.appendChild(li);
-
-
-
-
-
-
-
-//   if (i < 2) {
-//   child.textContent = ( (10 + i) +  ' am:' + ' ' + pikePlace.cookiesPerHour[i] + ' cookies');
-// } else if (i === 2) {
-//   cild.textContent = ('12 pm:' + ' ' + pikePlace.cookiesPerHour[i] + ' cookies');
-// } else {
-//   cild.textContent = ((i - 2) + ' pm:' + ' ' + pikePlace.cookiesPerHour[i] + ' cookies');
-// }
-//
-// total += pikePlace.cookiesPerHour[i];
-// parent.appendChild(child);
-// console.log(total);
+    li.textContent = hours[i] + ' ' + this.total[i] + ' cookies';
+    ulElement.appendChild(li);
     }
   }
 }
 
-pikePlaceMarket.render()
+pikePlaceMarket.customerPerHour();
+pikePlaceMarket.cookiesPerHour();
+pikePlaceMarket.render();
