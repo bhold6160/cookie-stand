4908d4d7b98fdc783a9ee3adc12c4463f9121fad
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-02-15 19:55:14 -0800

    working on form

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Function to calculate data going into table
 function CookieStand(name, min, max, avg) {
   this.name = name,
   this.min = min,
@@ -20,6 +21,7 @@ function CookieStand(name, min, max, avg) {
     }
   }
 
+// function to create the table
   this.renderCookiesPerHour = function() {
     this.cookiesPerHour();
     var table = document.getElementById('table');
@@ -31,34 +33,67 @@ function CookieStand(name, min, max, avg) {
 
     for (i = 0; i < this.hours.length; i++) {
       var td = document.createElement('td');
-      td.textContent = this.totalArr[i] + ' Cookies';
+      td.textContent = this.totalArr[i] + ' Customers';
       tr.appendChild(td);
     }
-    var tableData = document.createElement('td');
-    tableData.textContent = this.total + ' Cookies';
-    tr.appendChild(tableData);
+    var tableTotal = document.createElement('td');
+    tableTotal.textContent = this.total + ' Cookies';
+    tr.appendChild(tableTotal);
   }
 }
 
+// Creating new variables to add to table
 var pikePlace = new CookieStand('Pike Place Market ', 17, 88, 5.2);
 var seatacAirport = new CookieStand('SeaTac Airport ', 6, 24, 1.2);
 var southCenter = new CookieStand('SouthCenter ', 11, 38, 1.9);
 var belleSquare = new CookieStand('Bellevue Square ', 20, 48, 3.3);
 var alki = new CookieStand('Alki ', 3, 24, 2.6);
 
+//calling each new variable and adding it to the table
 pikePlace.renderCookiesPerHour();
 seatacAirport.renderCookiesPerHour();
 southCenter.renderCookiesPerHour();
 belleSquare.renderCookiesPerHour();
 alki.renderCookiesPerHour();
 
-
+// User input form
 var userForm = getElementById('userForm');
 var submitButton = getElementById('submitButton');
 
 function formSubmit() {
- var userData = document.createElement('tr')
+ var userData = document.createElement('tr');
 
- var userStore = document.createElement('td')
- userStore.textContent
+ var userStore = document.createElement('td');
+ userStore.textContent = this.name;
+ tr.appendChild(userStore);
+
+ var userArr = document.createElement('td');
+ userArr.textContent = this.totalArr[i] + ' Customers';
+ tr.appendChild(userArr)
+
+ var userTotal = document.createElement('td')
+ userTotal.textContent = this.total + ' Cookies';
+ tr.appendChild(userTotal)
 }
+
+// Handling event
+function handleFormSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+
+  var userName = event.target.name.value;
+  var userMin = parseFloat(event.target.min.value);
+  var userMax = parseFloat(event.target.max.value);
+  var userAvg = parseFloat(event.target.avg.value);
+
+  var userNew = new CookieStand(name, min, max, avg);
+  userNew.renderCookiesPerHour();
+
+  newItemRow(userNew);
+
+}
+
+table.addEventListener('submitButton', formSubmit);
+
+formSubmit();
+renderCookiesPerHour();
